498766696827f87f70fa60c68fb59231a53c5073
chrimaho <44449504+chrimaho@users.noreply.github.com>
2025-09-01 07:30:08 +1000

    Update Code Coverage info

--- src/utils/scripts.py
+++ src/utils/scripts.py
@@ -186,7 +186,8 @@ def git_switch_to_docs_branch() -> None:
 
 
 def git_add_coverage_report() -> None:
-    run("cp -r ./cov-report/html/* ./docs/code/coverage/")
+    run("mkdir -p ./docs/code/coverage/")
+    run("cp -r ./cov-report/html/. ./docs/code/coverage/")
     run("git add ./docs/code/coverage/")
     run(
         "git commit --no-verify",
