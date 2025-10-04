d297f9729775cf53562531e1a7c7dc57a1d042fb
chrimaho <44449504+chrimaho@users.noreply.github.com>
2025-10-04 12:24:38 +1000

    Fix bug

--- src/docstring_format_checker/__init__.py
+++ src/docstring_format_checker/__init__.py
@@ -16,7 +16,7 @@ _metadata = metadata("docstring-format-checker")
 __name__: str = _metadata["Name"]
 __version__: str = _metadata["Version"]
 __author__: str = _metadata["Author"]
-__email__: str = _metadata["Email"]
+__email__: str = _metadata.get("Email", "")
 
 __all__: list[str] = [
     "DocstringChecker",
